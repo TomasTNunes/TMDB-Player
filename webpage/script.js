fe9f892ad1d4d527002cdf3b2d44adc99a7f278a
TomasTNunes <tomastrindadenunes@outlook.com>
2025-02-09 20:58:25 +0100

    feat(webpage): add javascript to title and next episode

--- webpage/script.js
+++ webpage/script.js
@@ -47,17 +47,102 @@ function changeServer(serverNumber) {
     document.getElementById('videoFrame').src = src;
 
     // Highlight the selected server button
-    const buttons = document.querySelectorAll('.buttons button');
+    const buttons = document.querySelectorAll('.server-grid button');
     buttons.forEach(button => button.classList.remove('selected'));
     document.getElementById(`server${serverNumber}`).classList.add('selected');
 }
 
-// Redirect or load default server on page load
-window.onload = () => {
+async function fetchTMDBData(params) {
+    const result = {};
+
+    try {
+        let url;
+        const headers = {
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYTk1NzRmZDcxMjRkNmI5ZTUyNjA4ZWEzNWQ2NzdiNCIsIm5iZiI6MTczNzU5MDQ2NC4zMjUsInN1YiI6IjY3OTE4NmMwZThiNjdmZjgzM2ZhNjM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kWqK74FSN41PZO7_ENZelydTtX0u2g6dCkAW0vFs4jU`,
+            'accept': 'application/json'
+        };
+
+        if (params.type === 'movie') {
+            url = `https://api.themoviedb.org/3/movie/${params.id}?language=en-US`;
+            const response = await fetch(url, { method: 'GET', headers: headers });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            result['title'] = data.original_title;
+        } else if (params.type === 'tv') {
+            url = `https://api.themoviedb.org/3/tv/${params.id}?language=en-US`;
+            const response = await fetch(url, { method: 'GET', headers: headers });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            result['title'] = data.name;
+            const seasons = data.seasons;
+            for (const season of seasons) {
+                result[season.season_number] = season.episode_count;
+            }
+        } else {
+            throw new Error('Invalid type specified');
+        }
+        return result;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Re-throw the error if you want the caller to handle it
+    }
+}
+
+function getNextEp(currentSeason, currentEpisode, tmdbData) {
+    const currentSeasonEps = tmdbData[currentSeason];
+    if (currentEpisode < currentSeasonEps) {
+        return [parseInt(currentSeason), parseInt(currentEpisode) + 1];
+    }
+    const nextSeasonEps = tmdbData[parseInt(currentSeason) + 1];
+    if (nextSeasonEps !== undefined) {
+        return [parseInt(currentSeason) + 1, 1];
+    }
+    return [null, null];
+}
+
+window.onload = async () => {
     const params = getURLParams();
     if (!params) {
         redirectTowebsite();
-    } else {
-        changeServer(1);
+        return;
     }
+
+    try {
+        const tmdbData = await fetchTMDBData(params);
+        console.log(tmdbData.title);
+
+        if (params.type === 'movie') {
+            document.getElementById('title').innerText = `${tmdbData.title}`;
+        } else {
+            document.getElementById('title').innerText = `${tmdbData.title} S${params.season} E${params.episode}`;
+
+            const [nextEpS, nextEpE] = getNextEp(params.season, params.episode, tmdbData);
+            if (nextEpS !== null) {
+                const nextEpButton = document.getElementById('nextep-button');
+                nextEpButton.title = `Next Episode: S${nextEpS} E${nextEpE}`;
+                nextEpButton.style.display = 'flex'; 
+                nextEpButton.disabled = false;
+                nextEpButton.addEventListener('click', () => {
+                    const nextEpisodeUrl = `https://tmdb.nunesnetwork.com/?type=tv&id=${params.id}&s=${nextEpS}&e=${nextEpE}`;
+                    window.location.href = nextEpisodeUrl;
+                });
+            }
+            else {
+                const nextEpButton = document.getElementById('nextep-button');
+                nextEpButton.title = `No Next Episode`;
+                nextEpButton.style.display = 'flex'; // Ensure the button is visible
+                nextEpButton.disabled = true; // Disable the button
+            }
+        }
+
+    } catch (error) {
+        console.error('Error loading data:', error);
+        // Optionally, display an error message to the user
+        document.getElementById('title').innerText = 'Title';
+    }
+    changeServer(1);
 };
