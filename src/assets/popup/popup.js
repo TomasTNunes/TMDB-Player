fc7e770153bb95a529bcc20261454b86233074f7
TomasTNunes <tomastrindadenunes@outlook.com>
2025-02-12 05:24:51 +0100

    style(popup): improve popup layout (html and css)

--- src/assets/popup/popup.js
+++ src/assets/popup/popup.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('goToTmdb').addEventListener('click', () => {
+    document.getElementById('tmdb-button').addEventListener('click', () => {
         chrome.tabs.create({ url: 'https://www.themoviedb.org/' });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('git-button').addEventListener('click', () => {
+        chrome.tabs.create({ url: 'https://github.com/TomasTNunes/TMDB-Player/tree/master?tab=readme-ov-file#tmdb-player' });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('bug-button').addEventListener('click', () => {
+        chrome.tabs.create({ url: 'https://github.com/TomasTNunes/TMDB-Player/issues' });
     });
 });
